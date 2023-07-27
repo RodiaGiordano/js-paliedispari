e8c240dc2adc1e924148e0c0f0c0fe27045f9480
RodiaGiordano <rodiagiordano93@gmail.com>
2023-07-27 18:17:10 +0200

    fix: function "ciao()"

--- js/script.js
+++ js/script.js
@@ -1,79 +1,74 @@
-// let userWord ="";
+ let userWord ="";
 
 
-// const inputUser = prompt("inserisci una parola");
+const inputUser = prompt("inserisci una parola");
 
 
 
-// function ciao(){
-//     for (let i = inputUser.length -1; i >= 0; i--){
+ function stringReverse(){
+     for (let i = inputUser.length -1; i >= 0; i--){
 
     
-//             userWord += inputUser[i];
+        userWord += inputUser[i];
+    
+        console.log(userWord);
             
-//             console.log(userWord);
-            
-//         }
+         }
         
-//         if(inputUser === userWord){
-//             alert("la parola è palindroma");
-//         }
+         if(inputUser === userWord){
+             alert("la parola è palindroma");
+         }
         
-//         else{
-//             alert("la parola non è palindroma");
-//         }
-//         return(userWord);
-        
+         else{
+             alert("la parola non è palindroma");
+         }    
+ }
+
+ stringReverse();
+
+
+
+// const max = 5;
+// const min = 1;
+
+// const userChoice = prompt("inserisci paro o disparo")
+
+// const userNumber = parseInt(prompt("inserisci un numero compreso fra " + max + " e " + min))
+
+
+// console.log("scelta utente: " + userChoice);
+// console.log("scelta numero utente: " + userNumber);
+
+
+// function randomNumber() {
+
+// let random = 0;
+// random = Math.floor(Math.random() * max) + min ;
+//  return random   
 // }
 
-// ciao();
+// const random = randomNumber();
 
+// console.log("numero CPU: " + random);
 
-
-const max = 5;
-const min = 1;
-
-
-const userChoice = prompt("inserisci paro o disparo")
-
-const userNumber = parseInt(prompt("inserisci un numero compreso fra " + max + " e " + min))
-
-
-
-console.log("scelta utente: " + userChoice);
-console.log("scelta numero utente: " + userNumber);
-
-
-
-
-function randomNumber() {
-
-let random = 0;
-random = Math.floor(Math.random() * max) + min ;
-
-return random
-    
-}
-
-const random = randomNumber();
-
-console.log("numero CPU: " + random);
-
- function result(){
+//  function result(){
      
-    let numbersResult = random + userNumber;
+//     let numbersResult = random + userNumber;
     
-    return numbersResult;
- }
+//     return numbersResult;
+//  }
 
-numbersResult = result();
+// numbersResult = result();
 
- console.log("somma: " + numbersResult)
+//  console.log("somma: " + numbersResult)
 
- if(numbersResult % 2 == 0 && userChoice == "paro" || (numbersResult % 2 !== 0 && userChoice == "disparo")){
-    alert("l'utente ha vinto")
- }
-    else{
-        alert("il Bot ha vinto")
-    }
+//  if(numbersResult % 2 == 0 && userChoice == "paro" || (numbersResult % 2 !== 0 && userChoice == "disparo")){
+//     alert("l'utente ha vinto")
+//  }
+//     else{
+//         alert("il Bot ha vinto")
+//     }
  
+// console.log(result())
+// console.log(result())
+// console.log(result())
