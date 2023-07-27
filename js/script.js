8fbed11617acd9fd756321c7c2934bb90d273a9b
RodiaGiordano <rodiagiordano93@gmail.com>
2023-07-27 16:06:29 +0200

    feat: add function palindromo

--- js/script.js
+++ js/script.js
@@ -1,27 +1,32 @@
-const inputUser = prompt("inserisci una parola");
-
-console.log("parola: " + inputUser)
-// console.log(inputUser.length)
-
-max = inputUser.length;
-
 let userWord ="";
 
-for (let i = inputUser.length - 1; i >= 0; i--){
+
+const inputUser = prompt("inserisci una parola");
+
+
+
+function ciao(){
+    for (let i = inputUser.length - 1; i >= 0; i--){
 
     
-    userWord += inputUser[i];
-    
-    console.log(userWord);
-    
+            userWord += inputUser[i];
+            
+            console.log(userWord);
+            
+        }
+        
+        if(inputUser === userWord){
+            alert("la parola è palindroma");
+        }
+        
+        else{
+            alert("la parola non è palindroma");
+        }
+        return(userWord);
+        
 }
 
-if(inputUser === userWord){
-    alert("la parola è palindroma")
-}
+ciao();
 
-else{
-    alert("la parola non è palindroma")
-}
 
 
